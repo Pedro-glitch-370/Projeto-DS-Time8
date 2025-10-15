78721cbd45a9fe6c9f905578b0e06d1e5552d109
Bruno Melo <brunosouzal.melo@hotmail.com>
2025-10-14 23:53:28 -0300

    Update main.jsx

--- RPG/src/main.jsx
+++ RPG/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/* Importa o CSS e JS para a barra superior (top nav) */
+import './components/barra-superior/barra-superior.css'
+import './components/barra-superior/barra-superior.js'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
