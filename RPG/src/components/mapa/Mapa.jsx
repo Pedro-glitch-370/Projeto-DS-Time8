82b3756905a4bdff8b448c88f79dfb5b7306afb4
Pedro-glitch-370 <pedroh7.brito@gmail.com>
2025-10-17 11:49:57 -0300

    mapa tá indo

---
+++ RPG/src/components/mapa/Mapa.jsx
@@ -0,0 +1,15 @@
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default function Mapa() {
+  return (
+    <>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '500px', width: '100%' }}>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+    </MapContainer>
+    </>
+  )
+}
