12bce7692e54d9f889280b576fb8ca323ee9531e
Pedro-glitch-370 <pedroh7.brito@gmail.com>
2025-12-10 15:31:23 -0300

    TRUST THE PROCESS :?AASD

--- RPG/src/components/barra-superior/popups/RegisterPopUp.jsx
+++ RPG/src/components/barra-superior/popups/RegisterPopUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { isValidEmail } from "../para-react/isValidEmail";
+import "./registerPopUp.css";
 
 export default function RegisterPopup({ onClose, abrirLogin }) {
   const [nome, setNome] = useState("");
